c665621f366b8b1a03594785c46c4058540f41c6
Roman <romchikus1987@gmail.com>
2023-06-05 17:20:09 +0300

    updated

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,13 +3,13 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = (request) => {
+/* const getTokenFrom = (request) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.startsWith('Bearer ')) {
     return authorization.replace('Bearer ', '')
   }
   return null
-}
+} */
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -36,15 +36,12 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'url is missing' })
   }
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
 
   const user = await User.findById(decodedToken.id)
-  if (!user) {
-    return response.status(404).json({ error: 'User not found' })
-  }
 
   const blog = new Blog({
     title: body.title,
