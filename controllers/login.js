22153c94b0ceb1377504a8b2c0d7f9c6c1eb380b
Roman <romchikus1987@gmail.com>
2023-06-14 14:19:32 +0300

    updated

--- controllers/login.js
+++ controllers/login.js
@@ -6,23 +6,22 @@ const User = require('../models/user')
 loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body
 
-  if (!password) {
-    return response.status(400).json({
-      error: 'Password is required',
-    })
-  }
-
   const user = await User.findOne({ username })
 
   const passwordCorrect =
     user === null ? false : await bcrypt.compare(password, user.passwordHash)
 
-  if (!(user && passwordCorrect)) {
+  if (!user) {
     return response.status(401).json({
-      error: 'invalid username or password',
+      error: 'invalid username',
     })
   }
 
+  if (!passwordCorrect) {
+    return response.status(401).json({
+      error: 'invalid password',
+    })
+  }
   const userForToken = {
     username: user.username,
     id: user.id,
